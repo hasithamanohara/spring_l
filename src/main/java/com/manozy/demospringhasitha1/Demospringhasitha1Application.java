a4a181054056e4ab61e5bb0205b1125ab445454d
hasithauom <https://github.com/hasithauom/safty_notification_system-Back-End-Springboot-Mobile.git>
2023-10-23 01:32:34 +0530

    initial commit of springl

---
+++ src/main/java/com/manozy/demospringhasitha1/Demospringhasitha1Application.java
@@ -0,0 +1,13 @@
+package com.manozy.demospringhasitha1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demospringhasitha1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demospringhasitha1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/manozy/demospringhasitha1/Demospringhasitha1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manozy.demospringhasitha1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demospringhasitha1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
